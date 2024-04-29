eb17e1383333b6f3fa3600f97de85c6c7be89797
thavao <othavioact@gmail.com>
2024-04-29 16:07:21 -0300

    Add project files.

---
+++ EstanteLivro_OO/Livro.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Livro
+{
+    public string titulo;
+    public string autores;
+    public DateOnly dataPublicacao;
+    public string editora;
+    public string edicao;
+    public string ISBN;
+    public int quantidadePaginas;
+
+    public Livro()
+    {
+
+    }
+    public void DefinirTitulo(string t)
+    {
+        this.titulo = t;
+    }
+    public void DefinirAutores(string a)
+    {
+        this.autores = a;
+    }
+    public void DefinirDataPublicacao(DateOnly d)
+    {
+        this.dataPublicacao = d;
+    }
+    public void DefinirEditora(string e)
+    {
+        this.editora = e;
+    }
+    public void DefinirEdicao(string ed)
+    {
+        this.edicao = ed;
+    }
+    public void DefinirISBN(string i)
+    {
+        this.ISBN = i;
+    }
+    public void DefinirQtdPaginas(int p)
+    {
+        this.quantidadePaginas = p;
+    }
+
+    public void ImprimirLivro()
+    {
+        Console.WriteLine("Informações do Livro:");
+        Console.WriteLine($"Título: {this.titulo}");
+        Console.WriteLine($"Autores: {this.autores}");
+        Console.WriteLine($"Data de Publicação: {this.dataPublicacao}");
+        Console.WriteLine($"Editora: {this.editora}");
+        Console.WriteLine($"Edição: {this.edicao}");
+        Console.WriteLine($"ISBN: {this.ISBN}");
+        Console.WriteLine($"Quantidade de Páginas: {this.quantidadePaginas}");
+    }
+}
---
+++ EstanteLivro_OO/Program.cs
@@ -0,0 +1,94 @@
+﻿int tamanhoEstante, opcao;
+
+
+Livro CriarLivro()
+{
+    Livro novoLivro = new Livro();
+
+    Console.Write("Digite o nome do livro: ");
+    novoLivro.DefinirTitulo(Console.ReadLine());
+
+    Console.Write("Digite o autor do livro: ");
+    novoLivro.DefinirAutores(Console.ReadLine());
+
+    Console.Write("Digite a data de publicação do livro: ");
+    novoLivro.DefinirDataPublicacao(DateOnly.Parse(Console.ReadLine()));
+
+    Console.Write("Digite a editora do livro: ");
+    novoLivro.DefinirEditora(Console.ReadLine());
+
+    Console.Write("Digite a edição do livro: ");
+    novoLivro.DefinirEdicao(Console.ReadLine());
+
+    Console.Write("Digite o ISBN do livro: ");
+    novoLivro.DefinirISBN(Console.ReadLine());
+
+    Console.Write("Digite a quantidae de páginas do livro: ");
+    novoLivro.DefinirQtdPaginas(int.Parse(Console.ReadLine()));
+
+
+    return novoLivro;
+}
+
+Livro[] InserirLivrosNaEstante(Livro[] es)
+{
+    for (int i = 0; i < tamanhoEstante; i++)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Cadastre o {i + 1}º livro");
+        es[i] = CriarLivro();
+        Console.WriteLine();
+    }
+    return es;
+}
+
+void ImprimirEstante(Livro[] es)
+{
+    Console.WriteLine();
+    Console.WriteLine("----Livros na estante----");
+    for (int i = 0; i < tamanhoEstante; i++)
+    {
+        Console.WriteLine("---------------------------------");
+        es[i].ImprimirLivro();
+    }
+}
+void ImprimirLivroEspecifico(Livro[] es, int indice)
+{
+    es[indice].ImprimirLivro();
+}
+
+
+
+Console.Write("Defina o tamanho da estante: ");
+tamanhoEstante = int.Parse(Console.ReadLine());
+
+Livro[] estante = new Livro[tamanhoEstante];
+
+
+do
+{
+    Console.WriteLine("Escolha uma opção: ");
+    Console.WriteLine("1 - Inserir livros na estante");
+    Console.WriteLine("2 - Ver livros da estante");
+    Console.WriteLine("3 - Ver 1 livro da estante");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            estante = InserirLivrosNaEstante(estante);
+            break;
+        case 2:
+            ImprimirEstante(estante);
+            break;
+        case 3:
+            Console.WriteLine("Digite a posição do livro na estante");
+            ImprimirLivroEspecifico(estante, int.Parse(Console.ReadLine()) + 1);
+            break;
+        default:
+            Console.WriteLine("Opção inválida, tente novamente.");
+            break;
+    }
+    Console.WriteLine("Pressione Enter para continuar...");
+    Console.ReadLine();
+} while (true);
